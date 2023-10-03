7544a8bb98a3c3ceb825fdf05418bb63f623037a
Manish sahu <122163664+Manishs2261@users.noreply.github.com>
2023-10-03 22:22:45 +0530

    add a comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,75 +1,73 @@
- 
 
 <script>
- 
- 
+
+    // javascript code 
+
 
 </script>
 
-<!-- <BackgroundImage/> -->
+<!-- ================================================= -->
 
+<!-- for use backgroud color -->
 <div class="background-container">
     <!-- Your content goes here -->
-    <slot> 
-        </slot>
+    <slot />
 </div>
-  
+
+<!-- for Heading container Box  -->
 <center>
-    <div class="coun">
+    <div class="Heading_container">
         <center>
             <h2>Bulk Resize Images</h2>
         </center>
     </div>
+
+    <!-- for get a padding  -->
     <br /><br />
     <br /><br />
     <br /><br />
-    <div class="b">
+
+    <!-- Drop image here or image choose  -->
+    <div class="drop_image">
         <p>Drop images here <br /> or</p>
         <button type="button" class="bn" on:click={print}>Choose Images</button>
     </div>
 </center>
 
+<!-- for show a some features in website -->
+<div class="horizontal">
+    <div>Super-fast</div>
+    <div>100% free</div>
+    <div>Batch processing</div>
+    <div>Works in your web browser</div>
+</div>
 
-    <div class="horizontal">
-          
-        <div> Super-fast</div>
-       
-        <div>100% free</div>
-       
-        <div> Batch processing</div>
-        <div> Works in your web browser</div>
-        
-    </div>
+<div class="horizontal1">
+    <div>Easy to use</div>
+    <div>No sign-up</div>
+    <div>Private - no uploading</div>
+    <div>Compress Images</div>
+</div>
 
+<div class="horizontal2">
+    <div>Reduce file size of images</div>
+</div>
 
-
-    <div class="horizontal1">
-        <div>Easy to use</div>
-        <div>No sign-up</div>
-        <div>Private — no uploading</div>
-        <div>Compress Images</div>
-         
-    </div>
-
-
-    <div class="horizontal2">
-       
-        <div>Reduce file size of images</div>
-    </div>
- 
+<!-- CSS style section -->
 
 <style>
-    .coun {
+    /* heading Style */
+    .Heading_container {
         /* border: 2px solid red; */
         height: 40opx;
         width: 400px;
-      
+
         display: flex;
         justify-content: center;
         align-items: center;
         align-content: flex-end;
-        background-color:#AEBDCA;
-       border-radius: 8px;
+        background-color: #aebdca;
+        border-radius: 8px;
         /* box-shadow: 0px 20px 50px grey  ; */
         box-shadow: 0 8px 6px -6px rgb(36, 35, 35);
         /* object-fit: ; */
@@ -77,10 +75,12 @@
     h2 {
         font-size: 30px;
     }
-    .b {
+
+    /* drop image container style */
+    .drop_image {
         height: 200px;
         width: 1200px;
-        background-color:#AEBDCA;
+        background-color: #aebdca;
         border-radius: 8px;
         box-shadow: 0 8px 6px -6px rgb(36, 35, 35);
         align-content: flex-end;
@@ -88,8 +88,9 @@
         align-items: center;
         font-size: 28px;
         padding-top: 1%;
-        
     }
+
+    /* choose button style */
     .bn {
         height: 20%;
         width: 20%;
@@ -111,51 +112,32 @@
         transition: 400ms;
     }
 
+    /* website features style */
     .horizontal div {
         display: inline-block;
         padding-top: 7%;
         padding-left: 12%;
-         
-        
     }
     .horizontal1 div {
         display: inline-block;
         padding-top: 3%;
-
         padding-left: 12%;
-        
     }
     .horizontal2 div {
         display: inline-block;
         padding-top: 3%;
-
         padding-left: 12%;
-       
     }
-
 
     .background-container {
-      width: 100%;
-      height: 100%;
-     
-      background-size: cover;
-    
-    
-      position: absolute;
-    
-      background-color:#AEBDCA;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-   opacity: 40%;
-   /* text-align: start; */
-       
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        position: absolute;
+        background-color: #aebdca;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        opacity: 40%;
+        /* text-align: start; */
     }
-
-    img{
-        width: 7%;
-        
-        color: black;
-    }
-    
-
 </style>
